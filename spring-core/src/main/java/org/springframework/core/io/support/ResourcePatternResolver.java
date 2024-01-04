617dfa32c12ec39fdc149ea8ab4a32f3ddcc8feb
miqi1992 <1216938752@qq.com>
2024-01-04 22:21:25 +0800

    增加自己的项目

--- spring-core/src/main/java/org/springframework/core/io/support/ResourcePatternResolver.java
+++ spring-core/src/main/java/org/springframework/core/io/support/ResourcePatternResolver.java
@@ -56,15 +56,6 @@ import org.springframework.core.io.ResourceLoader;
 public interface ResourcePatternResolver extends ResourceLoader {
 
 	/**
-	 * Pseudo URL prefix for all matching resources from the class path: "classpath*:"
-	 * <p>This differs from ResourceLoader's classpath URL prefix in that it
-	 * retrieves all matching resources for a given name (e.g. "/beans.xml"),
-	 * for example in the root of all deployed JAR files.
-	 * @see org.springframework.core.io.ResourceLoader#CLASSPATH_URL_PREFIX
-	 */
-	String CLASSPATH_ALL_URL_PREFIX = "classpath*:";
-
-	/**
 	 * Resolve the given location pattern into {@code Resource} objects.
 	 * <p>Overlapping resource entries that point to the same physical
 	 * resource should be avoided, as far as possible. The result should
@@ -74,5 +65,14 @@ public interface ResourcePatternResolver extends ResourceLoader {
 	 * @throws IOException in case of I/O errors
 	 */
 	Resource[] getResources(String locationPattern) throws IOException;
+
+	/**
+	 * Pseudo URL prefix for all matching resources from the class path: "classpath*:"
+	 * <p>This differs from ResourceLoader's classpath URL prefix in that it
+	 * retrieves all matching resources for a given name (e.g. "/beans.xml"),
+	 * for example in the root of all deployed JAR files.
+	 * @see org.springframework.core.io.ResourceLoader#CLASSPATH_URL_PREFIX
+	 */
+	String CLASSPATH_ALL_URL_PREFIX = "classpath*:";
 
 }
